4a6e85c99f105c5f6133c21ebc24cdf6ff482190
Mauricio Serna <mauricioserf@gmail.com>
2018-01-19 14:28:24 -0500

    Variables and consts

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import "fmt"
 
+const helloWorld string = "Hi %s %s, welcome to the awesome Golang world!\n"
+const testConst = "Test"
+
 func main() {
 	var name string
+	lastname := "Serna"
+
+	var number = 100
+
+	var (
+		a = 1
+		b = 2
+		c = 3
+	)
 
 	fmt.Print("Enter your name: ")
 	fmt.Scanf("%s", &name)
-	fmt.Printf("Hi %s welcome to Bancolombia", name)
+	fmt.Printf(helloWorld, name, lastname)
+	fmt.Println(number, a, b, c, lastname)
 }
