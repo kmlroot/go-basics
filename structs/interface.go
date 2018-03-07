b2f67fdfe0b10834d787db887466e2f8c57d1cb2
Mauricio Serna <msflore@bancolombia.com>
2018-03-06 22:51:38 -0500

    [Add] defer.

--- structs/interface.go
+++ structs/interface.go
@@ -1,11 +1,20 @@
 package structs
 
+import (
+	"fmt"
+)
+
 // It's a interface of Course and Career
 type MyInterface interface {
 	Subscribe(name string)
 }
 
+func deferTest() {
+	fmt.Println("The function interface has finished")
+}
+
 func InterfaceTest() {
+	defer deferTest()
 	course := Course{Name: "Golang", Slug: "golang", Skills: []string{"1", "2"}}
 	career := new(Career)
 	career.Name = "Golang"
